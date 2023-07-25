7224c0117b66a29a573d8efd57fe111f20f6421b
melline prado <mellinevprado@hotmail.com>
2023-07-25 17:09:06 -0300

    fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,5 +12,5 @@ Flat.create!(
   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
   price_per_night: 75,
   number_of_guests: 3,
-  picture_url: 'https://images.unsplash.com/photo-1623298317883-6b70254edf31?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80'
+  picture_url: 'https://images.unsplash.com/photo-1494526585095-c41746248156?w=1200'
 )
